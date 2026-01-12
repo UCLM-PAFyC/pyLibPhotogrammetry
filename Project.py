32ce8c04dc5662fbe6dab073324de02d51f7b3dc
DHL <david.hernandez@uclm.es>
2026-01-12 09:53:23 +0100

    .

--- Project.py
+++ Project.py
@@ -1590,8 +1590,6 @@ class Project:
     def remove_process(self,
                        process_label):
         str_error = ''
-        self.sqls_to_process.clear()
-
         features_filters = []
         feature_filters = []
         filter = {}
