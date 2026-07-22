0a2a544da17113f0faa85b3b47aac95bb3f37d81
DHL <david.hernandez@uclm.es>
2026-07-22 19:01:41 +0200

    Develop refactor, first step

--- src/pyLibPhotogrammetry/__init__.py
+++ src/pyLibPhotogrammetry/__init__.py
@@ -1,6 +1,6 @@
-from defs import defs_graphos, defs_images, defs_metashape_markers, defs_processes, defs_project, defs_projects_dialog
-from gui import ProjectDefinitionDialog, PhotogrammetryProjectsDialog
-from core import ProjectPhotogrammetry
+from .defs import defs_graphos, defs_images, defs_metashape_markers, defs_processes, defs_project, defs_projects_dialog
+from .gui import ProjectDefinitionDialog, PhotogrammetryProjectsDialog
+from .core import ProjectPhotogrammetry
 __all__ = [
     "defs_graphos",
     "defs_images",
